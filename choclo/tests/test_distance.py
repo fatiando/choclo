2f14a32f5c7695e9de33d636ec7c5dc081acb418
Santiago Soler <santiago.r.soler@gmail.com>
2022-08-19 15:10:13 -0700

    Improve tests for distance functions

--- choclo/tests/test_distance.py
+++ choclo/tests/test_distance.py
@@ -7,50 +7,74 @@
 import numpy.testing as npt
 import pytest
 
+from .utils import dumb_spherical_distance
 from .._distance import distance_cartesian, distance_spherical, distance_spherical_core
 
 
-class TestDistanceCartesian:
-    @pytest.fixture(scope="class")
-    def point_a(self):
-        """
-        Define a sample point a
-        """
-        return (1.1, 1.2, 1.3)
-
-    @pytest.fixture(scope="class")
-    def point_b(self):
-        """
-        Define a sample point b
-        """
-        return (1.1, 1.2, 2.4)
-
-    def test_distance_cartesian(self, point_a, point_b):
-        """
-        Test if distance_cartesian works as expected
-        """
-        expected_value = 1.1
-        npt.assert_allclose(distance_cartesian(point_a, point_b), expected_value)
+@pytest.mark.parametrize(
+    "point_a, point_b, expected_distance",
+    [
+        ((1.1, 1.2, 1.3), (2.4, 1.2, 1.3), 1.3),
+        ((1.1, 1.2, 1.3), (1.1, -0.2, 1.3), 1.4),
+        ((1.1, 1.2, 1.3), (1.1, 1.2, -2.4), 3.7),
+        ((2.5, 3.4, -1.6), (8.7, -5.2, 0.4), 10.78888316740894),
+    ],
+)
+def test_distance_cartesian(point_a, point_b, expected_distance):
+    """
+    Test if distance_cartesian works as expected
+    """
+    npt.assert_allclose(distance_cartesian(point_a, point_b), expected_distance)
 
 
 class TestDistanceSpherical:
+    """
+    Tests for distance_spherical
+    """
+
+    @pytest.fixture(scope="class")
+    def point_equator(self):
+        """
+        Define a point in the equator of a sphere of radius 2
+        """
+        return (45, 0, 2)
+
+    @pytest.fixture(scope="class", params=[90, -90])
+    def point_pole(self, request):
+        """
+        Define a point in the equator of a sphere of radius 2
+        """
+        latitude = request.param
+        return (45, latitude, 2)
+
     @pytest.fixture(scope="class")
     def point_a(self):
         """
-        Define a sample point a
+        Define a sample point in spherical coordinates
         """
-        return (32.3, 40.1, 1e4)
+        return 35.6, -40.8, 10.3
 
     @pytest.fixture(scope="class")
     def point_b(self):
         """
-        Define a sample point b
+        Define a sample point in spherical coordinates
         """
-        return (32.3, 40.1, 1e4 + 100)
+        return 293.4, 70.9, 15.7
 
-    def test_distance_spherical(self, point_a, point_b):
+    def test_pole_and_equator(self, point_equator, point_pole):
         """
-        Test if distance_spherical works as expected
+        Test spherical distance using points in the equator and in the pole
         """
-        expected_value = 100
-        npt.assert_allclose(distance_spherical(point_a, point_b), expected_value)
+        expected_distance = 2 ** (3 / 2)
+        npt.assert_allclose(
+            distance_spherical(point_equator, point_pole), expected_distance
+        )
+
+    def test_sample_points(self, point_a, point_b):
+        """
+        Test spherical distance using two sample points
+        """
+        npt.assert_allclose(
+            distance_spherical(point_a, point_b),
+            dumb_spherical_distance(point_a, point_b),
+        )
---
+++ choclo/tests/utils.py
@@ -0,0 +1,73 @@
+"""
+Define utility functions for running the tests
+"""
+import numpy as np
+
+
+def spherical_to_geocentric_cartesian(longitude, latitude, radius):
+    """
+    Convert spherical coordinates into geocentric Cartesian coordinates
+
+    Parameters
+    ----------
+    longitude : float
+        Longitudinal coordinate (in degrees).
+    latitude : float
+        Latitudinal coordinate (in degrees).
+    radius : float
+        Radial coordinates (in meters).
+
+    Returns
+    -------
+    x, y, z : floats
+        Geocentric Cartesian coordinates of the passed point.
+    """
+    longitude, latitude = np.radians(longitude), np.radians(latitude)
+    x = radius * np.cos(longitude) * np.cos(latitude)
+    y = radius * np.sin(longitude) * np.cos(latitude)
+    z = radius * np.sin(latitude)
+    return x, y, z
+
+
+def dumb_spherical_distance(point_p, point_q):
+    """
+    Dumb calculation of distance between two points in spherical coordinates
+
+    This function converts the spherical coordinates of each point to
+    geocentric Cartesian coordinates and then uses a Cartesian Euclidean
+    distance calculation.
+
+    .. warning:
+
+        This implementation is less optimal than the one we have in
+        :func:`choclo.distance_spherical`. It exists only for testing purposes.
+
+    .. important:
+
+        All angles must be in degrees and radii in meters.
+
+    Parameters
+    ----------
+    point_p : tuple or 1d-array
+        Tuple or array containing the coordinates of the first point in the
+        following order: (``longitude``, ``latitude`` and ``radius``).
+        Both ``longitude`` and ``latitude`` must be in degrees, while
+        ``radius`` in meters.
+    point_q : tuple or 1d-array
+        Tuple or array containing the coordinates of the second point in the
+        following order: (``longitude``, ``latitude`` and ``radius``).
+        Both ``longitude`` and ``latitude`` must be in degrees, while
+        ``radius`` in meters.
+
+    Returns
+    -------
+    distance : float
+        Euclidean distance between ``point_p`` and ``point_q``.
+    """
+    # Convert points to geocentric Cartesian coordinates
+    point_p = spherical_to_geocentric_cartesian(*point_p)
+    point_q = spherical_to_geocentric_cartesian(*point_q)
+    # Convert points to arrays
+    point_p, point_q = np.asarray(point_p), np.asarray(point_q)
+    # Calculate Euclidean Cartesian distance between these two
+    return np.sqrt(np.sum((point_p - point_q) ** 2))
