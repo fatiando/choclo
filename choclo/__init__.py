6681db159139cd3b3038fe0f04b0a59aa41a45ac
Santiago Soler <santisoler@fastmail.com>
2022-10-25 15:28:32 -0700

    Import dipole in __init__.py to make the module available

--- choclo/__init__.py
+++ choclo/__init__.py
@@ -5,5 +5,5 @@
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 #
 # Import functions/classes to make the public API
-from . import point, prism
+from . import point, prism, dipole
 from ._version import __version__
