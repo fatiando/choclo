008c8fff8892176e20dd09244ef2f44024e14984
Santiago Soler <santisoler@fastmail.com>
2022-11-08 14:33:09 -0800

    Add symmetry test for magnetic_n and magnetic_e

--- choclo/tests/test_prism_magnetic.py
+++ choclo/tests/test_prism_magnetic.py
@@ -11,7 +11,7 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
-from ..prism import magnetic_u
+from ..prism import magnetic_e, magnetic_n, magnetic_u
 
 
 @pytest.fixture(name="sample_prism")
@@ -66,6 +66,396 @@ def get_prism_center(prism):
     northing = (prism[2] + prism[3]) / 2
     upward = (prism[4] + prism[5]) / 2
     return easting, northing, upward
+
+
+class TestSymmetryBe:
+    """
+    Test symmetry of easting component of the magnetic field
+    """
+
+    atol = 1e-17  # absolute tolerance for values near zero
+
+    @pytest.mark.parametrize(
+        "magnetization",
+        [
+            (500, 0, 0),
+            (-500, 0, 0),
+            (0, 500, 0),
+            (0, -500, 0),
+            (0, 0, 500),
+            (0, 0, -500),
+        ],
+    )
+    def test_symmetry_across_easting_northing(
+        self, sample_3d_grid, sample_prism, magnetization
+    ):
+        """
+        Test symmetry of magnetic_e across the easting-northing plane that
+        passes through the center of the prism
+        """
+        easting, northing, upward = sample_3d_grid
+        # Keep only the observation points that are above the prism center
+        _, _, prism_center_u = get_prism_center(sample_prism)
+        is_top = upward > prism_center_u
+        easting = easting[is_top]
+        northing = northing[is_top]
+        upward_top = upward[is_top]
+        # Create a symmetrical upward coordinate for points below the prism
+        # center
+        upward_bottom = 2 * prism_center_u - upward_top
+        # Compute magnetic_e on every observation point
+        magnetization = np.array(magnetization)
+        b_e_top = np.array(
+            list(
+                magnetic_e(e, n, u, sample_prism, magnetization)
+                for e, n, u in zip(
+                    easting.ravel(), northing.ravel(), upward_top.ravel()
+                )
+            )
+        )
+        b_e_bottom = np.array(
+            list(
+                magnetic_e(e, n, u, sample_prism, magnetization)
+                for e, n, u in zip(
+                    easting.ravel(), northing.ravel(), upward_bottom.ravel()
+                )
+            )
+        )
+        # Check symmetry between top and bottom
+        if magnetization[2] != 0:
+            npt.assert_allclose(b_e_top, -b_e_bottom, atol=self.atol)
+        else:
+            npt.assert_allclose(b_e_top, b_e_bottom, atol=self.atol)
+
+    @pytest.mark.parametrize(
+        "magnetization",
+        [
+            (500, 0, 0),
+            (-500, 0, 0),
+            (0, 500, 0),
+            (0, -500, 0),
+            (0, 0, 500),
+            (0, 0, -500),
+        ],
+    )
+    def test_symmetry_across_easting_upward(
+        self, sample_3d_grid, sample_prism, magnetization
+    ):
+        """
+        Test symmetry of magnetic_e across the easting-upward plane that
+        passes through the center of the prism
+        """
+        easting, northing, upward = sample_3d_grid
+        # Keep only the observation points that are north the prism center
+        _, prism_center_n, _ = get_prism_center(sample_prism)
+        is_north = northing > prism_center_n
+        easting = easting[is_north]
+        northing_north = northing[is_north]
+        upward = upward[is_north]
+        # Create a symmetrical upward coordinate for points south the prism
+        # center
+        northing_south = 2 * prism_center_n - northing_north
+        # Compute magnetic_e on every observation point
+        magnetization = np.array(magnetization)
+        b_e_north = np.array(
+            list(
+                magnetic_e(e, n, u, sample_prism, magnetization)
+                for e, n, u in zip(
+                    easting.ravel(), northing_north.ravel(), upward.ravel()
+                )
+            )
+        )
+        b_e_south = np.array(
+            list(
+                magnetic_e(e, n, u, sample_prism, magnetization)
+                for e, n, u in zip(
+                    easting.ravel(), northing_south.ravel(), upward.ravel()
+                )
+            )
+        )
+        # Check symmetry between south and north
+        if magnetization[1] != 0:
+            npt.assert_allclose(b_e_north, -b_e_south, atol=self.atol)
+        else:
+            npt.assert_allclose(b_e_north, b_e_south, atol=self.atol)
+
+    @pytest.mark.parametrize(
+        "magnetization",
+        [
+            (500, 0, 0),
+            (-500, 0, 0),
+            (0, 500, 0),
+            (0, -500, 0),
+            (0, 0, 500),
+            (0, 0, -500),
+        ],
+    )
+    def test_symmetry_across_northing_upward(
+        self, sample_3d_grid, sample_prism, magnetization
+    ):
+        """
+        Test symmetry of magnetic_e across the northing-upward plane that
+        passes through the center of the prism
+        """
+        easting, northing, upward = sample_3d_grid
+        # Keep only the observation points that are east the prism center
+        prism_center_e, _, _ = get_prism_center(sample_prism)
+        is_east = easting > prism_center_e
+        easting_east = easting[is_east]
+        northing = northing[is_east]
+        upward = upward[is_east]
+        # Create a symmetrical upward coordinate for points west the prism
+        # center
+        easting_west = 2 * prism_center_e - easting_east
+        # Compute magnetic_e on every observation point
+        magnetization = np.array(magnetization)
+        b_e_east = np.array(
+            list(
+                magnetic_e(e, n, u, sample_prism, magnetization)
+                for e, n, u in zip(
+                    easting_east.ravel(), northing.ravel(), upward.ravel()
+                )
+            )
+        )
+        b_e_west = np.array(
+            list(
+                magnetic_e(e, n, u, sample_prism, magnetization)
+                for e, n, u in zip(
+                    easting_west.ravel(), northing.ravel(), upward.ravel()
+                )
+            )
+        )
+        # Check symmetry between west and east
+        if magnetization[0] != 0:
+            npt.assert_allclose(b_e_east, b_e_west, atol=self.atol)
+        else:
+            npt.assert_allclose(b_e_east, -b_e_west, atol=self.atol)
+
+    def test_symmetry_when_flipping(self, sample_3d_grid, sample_prism):
+        """
+        Test symmetry of magnetic_e when flipping its direction
+        """
+        easting, northing, upward = sample_3d_grid
+        # Keep only points with easting on east of the prism center
+        prism_center_e, _, _ = get_prism_center(sample_prism)
+        is_east_or_equal = easting >= prism_center_e
+        easting = easting[is_east_or_equal]
+        northing = northing[is_east_or_equal]
+        upward = upward[is_east_or_equal]
+        # Define two magnetic moments
+        magnetization_east = np.array([500.0, 0, 0])
+        magnetization_west = np.array([-500.0, 0, 0])
+        # Compute the magnetic field generated by each moment on the
+        # observation points
+        b_e_east = np.array(
+            list(
+                magnetic_e(e, n, u, sample_prism, magnetization_east)
+                for e, n, u in zip(easting, northing, upward)
+            )
+        )
+        b_e_west = np.array(
+            list(
+                magnetic_e(e, n, u, sample_prism, magnetization_west)
+                for e, n, u in zip(easting, northing, upward)
+            )
+        )
+        # Check if the sign gets inverted
+        npt.assert_allclose(b_e_east, -b_e_west)
+
+
+class TestSymmetryBn:
+    """
+    Test symmetry of easting component of the magnetic field
+    """
+
+    atol = 1e-17  # absolute tolerance for values near zero
+
+    @pytest.mark.parametrize(
+        "magnetization",
+        [
+            (500, 0, 0),
+            (-500, 0, 0),
+            (0, 500, 0),
+            (0, -500, 0),
+            (0, 0, 500),
+            (0, 0, -500),
+        ],
+    )
+    def test_symmetry_across_easting_northing(
+        self, sample_3d_grid, sample_prism, magnetization
+    ):
+        """
+        Test symmetry of magnetic_n across the easting-northing plane that
+        passes through the center of the prism
+        """
+        easting, northing, upward = sample_3d_grid
+        # Keep only the observation points that are above the prism center
+        _, _, prism_center_u = get_prism_center(sample_prism)
+        is_top = upward > prism_center_u
+        easting = easting[is_top]
+        northing = northing[is_top]
+        upward_top = upward[is_top]
+        # Create a symmetrical upward coordinate for points below the prism
+        # center
+        upward_bottom = 2 * prism_center_u - upward_top
+        # Compute magnetic_n on every observation point
+        magnetization = np.array(magnetization)
+        b_n_top = np.array(
+            list(
+                magnetic_n(e, n, u, sample_prism, magnetization)
+                for e, n, u in zip(
+                    easting.ravel(), northing.ravel(), upward_top.ravel()
+                )
+            )
+        )
+        b_n_bottom = np.array(
+            list(
+                magnetic_n(e, n, u, sample_prism, magnetization)
+                for e, n, u in zip(
+                    easting.ravel(), northing.ravel(), upward_bottom.ravel()
+                )
+            )
+        )
+        # Check symmetry between top and bottom
+        if magnetization[2] != 0:
+            npt.assert_allclose(b_n_top, -b_n_bottom, atol=self.atol)
+        else:
+            npt.assert_allclose(b_n_top, b_n_bottom, atol=self.atol)
+
+    @pytest.mark.parametrize(
+        "magnetization",
+        [
+            (500, 0, 0),
+            (-500, 0, 0),
+            (0, 500, 0),
+            (0, -500, 0),
+            (0, 0, 500),
+            (0, 0, -500),
+        ],
+    )
+    def test_symmetry_across_easting_upward(
+        self, sample_3d_grid, sample_prism, magnetization
+    ):
+        """
+        Test symmetry of magnetic_n across the easting-upward plane that
+        passes through the center of the prism
+        """
+        easting, northing, upward = sample_3d_grid
+        # Keep only the observation points that are north the prism center
+        _, prism_center_n, _ = get_prism_center(sample_prism)
+        is_north = northing > prism_center_n
+        easting = easting[is_north]
+        northing_north = northing[is_north]
+        upward = upward[is_north]
+        # Create a symmetrical upward coordinate for points south the prism
+        # center
+        northing_south = 2 * prism_center_n - northing_north
+        # Compute magnetic_n on every observation point
+        magnetization = np.array(magnetization)
+        b_n_north = np.array(
+            list(
+                magnetic_n(e, n, u, sample_prism, magnetization)
+                for e, n, u in zip(
+                    easting.ravel(), northing_north.ravel(), upward.ravel()
+                )
+            )
+        )
+        b_n_south = np.array(
+            list(
+                magnetic_n(e, n, u, sample_prism, magnetization)
+                for e, n, u in zip(
+                    easting.ravel(), northing_south.ravel(), upward.ravel()
+                )
+            )
+        )
+        # Check symmetry between south and north
+        if magnetization[1] != 0:
+            npt.assert_allclose(b_n_north, b_n_south, atol=self.atol)
+        else:
+            npt.assert_allclose(b_n_north, -b_n_south, atol=self.atol)
+
+    @pytest.mark.parametrize(
+        "magnetization",
+        [
+            (500, 0, 0),
+            (-500, 0, 0),
+            (0, 500, 0),
+            (0, -500, 0),
+            (0, 0, 500),
+            (0, 0, -500),
+        ],
+    )
+    def test_symmetry_across_northing_upward(
+        self, sample_3d_grid, sample_prism, magnetization
+    ):
+        """
+        Test symmetry of magnetic_n across the northing-upward plane that
+        passes through the center of the prism
+        """
+        easting, northing, upward = sample_3d_grid
+        # Keep only the observation points that are east the prism center
+        prism_center_e, _, _ = get_prism_center(sample_prism)
+        is_east = easting > prism_center_e
+        easting_east = easting[is_east]
+        northing = northing[is_east]
+        upward = upward[is_east]
+        # Create a symmetrical upward coordinate for points west the prism
+        # center
+        easting_west = 2 * prism_center_e - easting_east
+        # Compute magnetic_n on every observation point
+        magnetization = np.array(magnetization)
+        b_n_east = np.array(
+            list(
+                magnetic_n(e, n, u, sample_prism, magnetization)
+                for e, n, u in zip(
+                    easting_east.ravel(), northing.ravel(), upward.ravel()
+                )
+            )
+        )
+        b_n_west = np.array(
+            list(
+                magnetic_n(e, n, u, sample_prism, magnetization)
+                for e, n, u in zip(
+                    easting_west.ravel(), northing.ravel(), upward.ravel()
+                )
+            )
+        )
+        # Check symmetry between west and east
+        if magnetization[0] != 0:
+            npt.assert_allclose(b_n_east, -b_n_west, atol=self.atol)
+        else:
+            npt.assert_allclose(b_n_east, b_n_west, atol=self.atol)
+
+    def test_symmetry_when_flipping(self, sample_3d_grid, sample_prism):
+        """
+        Test symmetry of magnetic_n when flipping its direction
+        """
+        easting, northing, upward = sample_3d_grid
+        # Keep only points with northing on north of the dipole
+        _, prism_center_n, _ = get_prism_center(sample_prism)
+        is_north_or_equal = northing >= prism_center_n
+        easting = easting[is_north_or_equal]
+        northing = northing[is_north_or_equal]
+        upward = upward[is_north_or_equal]
+        # Define two magnetic moments
+        magnetization_north = np.array([0, 500.0, 0])
+        magnetization_south = np.array([0, -500.0, 0])
+        # Compute the magnetic field generated by each moment on the
+        # observation points
+        b_n_north = np.array(
+            list(
+                magnetic_n(e, n, u, sample_prism, magnetization_north)
+                for e, n, u in zip(easting, northing, upward)
+            )
+        )
+        b_n_south = np.array(
+            list(
+                magnetic_n(e, n, u, sample_prism, magnetization_south)
+                for e, n, u in zip(easting, northing, upward)
+            )
+        )
+        # Check if the sign gets inverted
+        npt.assert_allclose(b_n_north, -b_n_south)
 
 
 class TestSymmetryBu:
@@ -143,7 +533,7 @@ class TestSymmetryBu:
     ):
         """
         Test symmetry of magnetic_e across the easting-upward plane that
-        passes through the location of the dipole
+        passes through the center of the prism
         """
         easting, northing, upward = sample_3d_grid
         # Keep only the observation points that are north of the prism center
@@ -195,10 +585,10 @@ class TestSymmetryBu:
     ):
         """
         Test symmetry of magnetic_e across the northing-upward plane that
-        passes through the location of the dipole
+        passes through the center of the prism
         """
         easting, northing, upward = sample_3d_grid
-        # Keep only the observation points that are east the dipole
+        # Keep only the observation points that are east of the prism center
         prism_center_e, _, _ = get_prism_center(sample_prism)
         is_east = easting > prism_center_e
         easting_east = easting[is_east]
@@ -236,7 +626,7 @@ class TestSymmetryBu:
         Test symmetry of magnetic_e when flipping its direction
         """
         easting, northing, upward = sample_3d_grid
-        # Keep only points with upward on top of the dipole
+        # Keep only points with upward on top of the prism center
         _, _, prism_center_u = get_prism_center(sample_prism)
         is_top_or_equal = upward >= prism_center_u
         easting = easting[is_top_or_equal]
