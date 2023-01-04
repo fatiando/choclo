019c8c353c499936ca0172268798d81d4041cdf0
Santiago Soler <santisoler@fastmail.com>
2023-01-03 16:32:56 -0800

    Implement singular points for diagonal tensor components
    Return nan when computing the diagonal tensor components on singular
    points (vertices or edges perpendicular to the direction of the tensor
    component). When computing on the greatest face (eastern,northern or top
    face) make the respective diagonal component to return the limit
    approaching from the outside. There's no need to change the
    implementation, just add a term of 4 pi to the result.

--- choclo/prism/_utils.py
+++ choclo/prism/_utils.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2022 The Choclo Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
 """
 Utility functions for forward modelling of prisms
 """
@@ -114,5 +120,121 @@ def is_point_on_upward_edge(easting, northing, upward, prism):
     in_northing = (northing == prism[2]) or (northing == prism[3])
     in_upward = prism[4] <= upward <= prism[5]
     if in_easting and in_northing and in_upward:
+        return True
+    return False
+
+
+@jit(nopython=True)
+def is_point_on_east_face(easting, northing, upward, prism):
+    """
+    Check if observation point falls in the eastern face of the prism
+
+    Return True if the observation point falls in the eastern face of the
+    prism, without including the vertices or the edges (the inside of the
+    face).
+
+    Parameters
+    ----------
+    easting : float
+        Easting coordinate of the observation point. Must be in meters.
+    northing : float
+        Northing coordinate of the observation point. Must be in meters.
+    upward : float
+        Upward coordinate of the observation point. Must be in meters.
+    prism : 1d-array
+        One dimensional array containing the coordinates of the prism in the
+        following order: ``west``, ``east``, ``south``, ``north``, ``bottom``,
+        ``top`` in a Cartesian coordinate system.
+        All coordinates should be in meters.
+
+    Returns
+    -------
+    result : bool
+        Return True if the observation point falls inside the eastern face of
+        the prism. Return False if otherwise.
+    """
+    on_east_face = (
+        (easting == prism[1])
+        and (prism[2] < northing < prism[3])
+        and (prism[4] < upward < prism[5])
+    )
+    if on_east_face:
+        return True
+    return False
+
+
+@jit(nopython=True)
+def is_point_on_north_face(easting, northing, upward, prism):
+    """
+    Check if observation point falls in the northern face of the prism
+
+    Return True if the observation point falls in the northern face of the
+    prism, without including the vertices or the edges (the inside of the
+    face).
+
+    Parameters
+    ----------
+    easting : float
+        Easting coordinate of the observation point. Must be in meters.
+    northing : float
+        Northing coordinate of the observation point. Must be in meters.
+    upward : float
+        Upward coordinate of the observation point. Must be in meters.
+    prism : 1d-array
+        One dimensional array containing the coordinates of the prism in the
+        following order: ``west``, ``east``, ``south``, ``north``, ``bottom``,
+        ``top`` in a Cartesian coordinate system.
+        All coordinates should be in meters.
+
+    Returns
+    -------
+    result : bool
+        Return True if the observation point falls inside the northern face of
+        the prism. Return False if otherwise.
+    """
+    on_north_face = (
+        (prism[0] < easting < prism[1])
+        and (northing == prism[3])
+        and (prism[4] < upward < prism[5])
+    )
+    if on_north_face:
+        return True
+    return False
+
+
+@jit(nopython=True)
+def is_point_on_top_face(easting, northing, upward, prism):
+    """
+    Check if observation point falls in the top face of the prism
+
+    Return True if the observation point falls in the top face of the prism,
+    without including the vertices or the edges (the inside of the face).
+
+    Parameters
+    ----------
+    easting : float
+        Easting coordinate of the observation point. Must be in meters.
+    northing : float
+        Northing coordinate of the observation point. Must be in meters.
+    upward : float
+        Upward coordinate of the observation point. Must be in meters.
+    prism : 1d-array
+        One dimensional array containing the coordinates of the prism in the
+        following order: ``west``, ``east``, ``south``, ``north``, ``bottom``,
+        ``top`` in a Cartesian coordinate system.
+        All coordinates should be in meters.
+
+    Returns
+    -------
+    result : bool
+        Return True if the observation point falls inside the top face of the
+        prism. Return False if otherwise.
+    """
+    on_top_face = (
+        (prism[0] < easting < prism[1])
+        and (prism[2] < northing < prism[3])
+        and (upward == prism[5])
+    )
+    if on_top_face:
         return True
     return False
