d62b8085c25737b05057946c5df180c106636441
Santiago Soler <santisoler@fastmail.com>
2024-09-24 10:39:55 -0700

    Improve docstring of test

--- choclo/tests/test_prism_magnetic.py
+++ choclo/tests/test_prism_magnetic.py
@@ -595,7 +595,7 @@ class TestDivergenceOfB:
         self, sample_3d_grid, sample_prism, sample_magnetization
     ):
         """
-        Compute the derivatives of B through finite differences.
+        Test divergence of B computing its derivatives through finite differences.
         """
         delta = 1e-6
         kwargs = dict(
