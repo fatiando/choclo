94e47ea7c80be6cf3cfdde1caf496e9851493eba
Santiago Soler <santisoler@fastmail.com>
2024-12-05 16:55:58 -0800

    Run make format

--- choclo/dipole/_forward.py
+++ choclo/dipole/_forward.py
@@ -3,6 +3,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
+# Copyright (c) 2022 The Choclo Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 """
 Magnetic forward modeling functions for a dipole.
 """
